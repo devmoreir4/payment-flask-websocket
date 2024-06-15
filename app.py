c0e284d533d6d29c31e807429119d719672bdace
Carlos Moreira <carlosmoreira.bji@gmail.com>
2024-06-15 15:51:35 -0300

    client disconnection from server

--- app.py
+++ app.py
@@ -78,8 +78,12 @@ def payment_pix_page(payment_id):
 
 # websockets
 @socketio.on('connect')
-def test_connect():
+def handle_connect():
     print('Client connected to the server')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client has disconnected to the server')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
